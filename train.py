b7143cf5d2fca2403f5d8ba4c773cdf7f3081d3b
Kid-key <fw.key@qq.com>
2020-10-30 10:53:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay')
 parser.add_argument('--print-freq', '-p', default=100, type=int,
                     metavar='N', help='print frequency')
